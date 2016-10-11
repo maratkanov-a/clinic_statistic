33c7aaaaa16447f230b62cbaf75587843d9a7ea7
maratkanov-a <a.s.maratkanov@gmail.com>
2016-10-11 18:47:28 +0300

    created skeleton with styles

---
+++ static/js/components/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+require('./../../css/content/__home/home.css');
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="content__home">
+                        <div className="hello twelve columns">
+                            <h3>
+                                Привет, странник, ты зашел на сайт такой-то, здесь очень круто, ты можешь пройти тест,
+                                посмотреть статистику по различным проектам
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+module.exports = Home;
--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,54 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from "react-router"
-import Home from './components/main'
+
+// custom modules
+import Home from './components/home'
 import Login from './components/login'
 import Test from './components/test'
 import Statistic from './components/test'
 
+// main styles
+require('./../css/lib/normalize.css');
+require('./../css/lib/skeleton.css');
+
+// bam styles
+require('./../css/header/header.css');
+require('./../css/content/content.css');
+require('./../css/footer/footer.css');
+
+
 class App extends React.Component {
     render() {
         return (
-            <div className="container">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">login</Link></li>
-                    <li><Link to="/test">test</Link></li>
-                    <li><Link to="/statistic">statistic</Link></li>
-                </ul>
-                {this.props.children}
+            <div>
+                <div className="header">
+                    <div className="container">
+                        <div className="logo three columns">
+                            НИИ статистика
+                        </div>
+                        <div className="elements seven columns">
+                            элементы
+                        </div>
+                        <div className="login two columns">
+                            <button className="button">
+                                <Link to="/login">Войти</Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/*<ul>*/}
+                    {/*<li><Link to="/">Home</Link></li>*/}
+                    {/*<li><Link to="/login">login</Link></li>*/}
+                    {/*<li><Link to="/test">test</Link></li>*/}
+                    {/*<li><Link to="/statistic">statistic</Link></li>*/}
+                {/*</ul>*/}
+                <div className="content">
+                    {this.props.children}
+                </div>
+                <div className="footer">
+
+                </div>
             </div>
         );
     }
